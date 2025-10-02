aae806d25ba6756624778676bfa0036a5ee6e4bb
hi-anki <managemee@protonmail.com>
2025-10-02 22:48:04 +0530

    node gen completed; prepared to be integrated with workflow; local only for testing; BUT THE GRAPH IS NOT WORKING, SO NOT INTEGRATING WITH WORKFLOW FOR NOW

--- scripts/node_gen.py
+++ scripts/node_gen.py
@@ -1,3 +1,6 @@
+# TO BE RUN BY WORKFLOW ONLY
+# LOCAL ONLY FOR TESTING
+
 import os, json
 
 ROOT_DIR = 'content/docs/'
@@ -10,42 +13,41 @@ mappings = {
 def xfm(path):
   with open(path) as fo:
     lines = fo.readlines()
-    return (lines[1].split("id: ")[1].rstrip('\n'), lines[2].split("title: ")[1].rstrip('\n'))
+    id = lines[1].split("id: ")[1].rstrip('\n')
+    title = lines[2].split("title: ")[1].rstrip('\n')
+    return (id, title)
 
 def update_json(mapp, path):
   with open(path, "w") as fo:
     json.dump(mapp, fo, indent=2)
 
 def main():
-  count = 0
   for cur_dir, _, files in os.walk(ROOT_DIR):
-    for f in files:
-      if f.endswith(".md"):
+    if 'extra' not in cur_dir:
+      for f in files:
+        if f.endswith(".md"):
 
-        # Populate node per file
-        path = os.path.join(cur_dir, f)
-        id, title = xfm(path)
+          # Populate node per file
+          path = os.path.join(cur_dir, f)
+          id, title = xfm(path)
 
-        if path.endswith("_index.md"):
-          path_ = '..' + path.lstrip('content').rstrip('_index.md')
-        else:
-          path_ = '..' + path.lstrip('content').rstrip('.md')
+          if path.endswith("_index.md"):
+            path_ = '..' + path.lstrip('content').rstrip('_index.md')
+          else:
+            path_ = '..' + path.lstrip('content').rstrip('.md')
 
-        node_ = {"id": id, "label": title, "path": path_}
-        mappings["nodes"].append(node_)
+          node_ = {"id": id, "label": title, "path": path_}
+          mappings["nodes"].append(node_)
 
-        # Populate edges per file, if any
-        with open(path) as fo:
-          lines = fo.readlines()
-          for line in lines:
-            if 'doclink' in line:
-              dest_id = line.split("doclink")[1].split('"')[1]
-              edge_ = {"source": id, "target": dest_id}
-              mappings["edges"].append(edge_)
-
-        count += 1
+          # Populate edges per file, if any
+          with open(path) as fo:
+            lines = fo.readlines()
+            for line in lines:
+              if 'doclink' in line:
+                dest_id = line.split("doclink")[1].split('"')[1]
+                edge_ = {"source": id, "target": dest_id}
+                mappings["edges"].append(edge_)
 
   update_json(mappings, 'static/graph/graph.json')
-  print(count)
 
 main()
